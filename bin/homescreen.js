61ad434a90ecb435cc5debe6040197e037876ea3
Daniel Krainas <dkrainas87@gmail.com>
2016-05-04 03:21:05 -0400

    fix script

--- bin/homescreen.js
+++ bin/homescreen.js
@@ -1,3 +1,16 @@
-#!/bin/env node
+#!/usr/bin/env node
 
-nw ../
+var app = require('child_process').spawn('node', [__dirname + '/../node_modules/.bin/nw', __dirname + '/..'], {
+	stdio: 'inherit',
+	shell: true
+});
+
+/*app.stdout.on('data', function (data) {
+	console.log(data);
+});*/
+
+app.on('close', function () {});
+
+app.on('error', function (err) {
+	console.error(`error: ${err}`);
+});
